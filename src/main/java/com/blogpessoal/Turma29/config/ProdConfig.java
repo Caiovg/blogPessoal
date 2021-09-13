e12eae92b190011d2764e607696274b3378e1b11
unknown <caioviniciusg.1000@gmail.com>
2021-09-13 11:15:14 -0300

    ProdConfig

---
+++ src/main/java/com/blogpessoal/Turma29/config/ProdConfig.java
@@ -0,0 +1,21 @@
+package com.blogpessoal.Turma29.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.blogpessoal.Turma29.services.DBService;
+
+@Configuration
+@Profile("prod")
+public class ProdConfig {
+
+	@Autowired
+	private DBService service;
+	
+	@Bean
+	public void instanciaDB() {	
+		this.service.instanceDB();
+	}
+}
