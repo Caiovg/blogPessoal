392fbf1c6670e1f7aace46eff08d501bcf42e9e8
unknown <caioviniciusg.1000@gmail.com>
2021-09-13 14:09:25 -0300

    Correções de bugs

--- src/main/java/com/blogpessoal/Turma29/Turma29Application.java
+++ src/main/java/com/blogpessoal/Turma29/Turma29Application.java
@@ -3,9 +3,13 @@ package com.blogpessoal.Turma29;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class Turma29Application {
 
 	@GetMapping
